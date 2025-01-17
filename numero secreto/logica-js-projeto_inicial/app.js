5ff8ef3e622627383f3b45a5870bb9b7555d8b26
Amarildo Chagas Junior <amarildo.chagasjr@gmail.com>
2025-01-16 22:03:07 -0400

    Add files via upload

---
+++ numero secreto/logica-js-projeto_inicial/app.js
@@ -0,0 +1,31 @@
+alert('Olá, tudo bem ?\nBoas vindas ao jogo do número secreto !!!');
+
+alert('Você tem 03 chances para acertar o número que vou escolher entre 1 e 15. Vamos lá ?\n\nVou pensar em um número...');
+
+let chances = 1
+
+let numeroSecreto = Math.floor((Math.random() * 15) + 1)
+console.log(numeroSecreto)
+
+let numeroEscolhido = prompt('Ok, ja escolhi meu número. Agora é sua vez, Digite um número entre 1 e 15:');
+
+
+while (chances < 4){
+    if (numeroEscolhido != numeroSecreto){
+        chances += 1;
+        if (chances === 4){
+            alert(`Que pena, você errou.\n\nO número secreto é ${numeroSecreto}.`);
+            break;
+        } else {
+            if (numeroEscolhido > numeroSecreto){
+                numeroEscolhido = prompt(`Ops, você errou, vamos para sua ${chances}ª. chance.\nUma dica: O número secreto é menor que ${numeroEscolhido}\nDigite um número:`);
+            } else {
+                numeroEscolhido = prompt(`Ops, você errou, vamos para sua ${chances}ª. chance.\nUma dica: O número secreto é maior que ${numeroEscolhido}\nDigite um número:`);
+            }
+        }
+    } else {
+        alert(`PARABÉNS !!!\n\nO número secreto é ${numeroSecreto}.`);
+        console.log('acertou');
+        break;
+    }  
+}
